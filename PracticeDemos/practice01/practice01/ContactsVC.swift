2b6475682d4a1a14b192a897f13b4ba01f5a4da7
刘涛 <>
2018-08-08 00:09:59 +0800

    new fix

--- PracticeDemos/practice01/practice01/ContactsVC.swift
+++ PracticeDemos/practice01/practice01/ContactsVC.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class ContactsVC: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.addSubview(scrollView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if #available(iOS 11.0, *) {
+            scrollView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.width, height: view.frame.height - view.safeAreaInsets.bottom - view.safeAreaInsets.top)
+        } else {
+            scrollView.frame = CGRect(x: 0, y: 200, width: view.frame.width, height: view.frame.height - topLayoutGuide.length - bottomLayoutGuide.length)
+        }
+        scrollView.contentSize = CGSize(width: self.view.frame.width, height: 800)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
