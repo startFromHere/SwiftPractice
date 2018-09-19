48058391ee7e14d5fe415455d709fdbc5c78959c
移动支付－刘涛 <liutao900629@gmail.com>
2018-09-19 10:34:27 +0800

    commit form gittest folder

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,6 +3,7 @@
 import UIKit
 
 //edit in test2
+//in git test folder, checkout from remote test1 branch(detached header)
 
 func inc( i: inout Int) -> () -> Int {
     return { i += 1; return i }  // 闭包中截获inout参数i
