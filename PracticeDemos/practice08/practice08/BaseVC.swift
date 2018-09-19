d482125325adfbafef233569f932d4f4f3b866bc
移动支付－刘涛 <liutao900629@gmail.com>
2018-09-19 09:46:29 +0800

    new files

---
+++ PracticeDemos/practice08/practice08/BaseVC.swift
@@ -0,0 +1,24 @@
+//
+//  BaseVC.swift
+//  practice08
+//
+//  Created by 移动支付－刘涛 on 2018/9/4.
+//  Copyright © 2018 移动支付－刘涛. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ PracticeDemos/practice08/practice08/Main/Discover/DiscoverVC.swift
@@ -0,0 +1,19 @@
+//
+//  DiscoverVC.swift
+//  practice08
+//
+//  Created by 移动支付－刘涛 on 2018/9/4.
+//  Copyright © 2018 移动支付－刘涛. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverVC: BaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
