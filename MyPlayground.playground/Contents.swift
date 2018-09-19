72e38c46b5f9f93412a49acda3871b716c3067d3
移动支付－刘涛 <liutao900629@gmail.com>
2018-09-19 10:36:34 +0800

    新分支 test2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,6 +5,8 @@ import UIKit
 //edit in test2
 //in git test folder, checkout from remote test1 branch(detached header)
 
+//在 gittest folder 中，添加新分支 test2
+
 func inc( i: inout Int) -> () -> Int {
     return { i += 1; return i }  // 闭包中截获inout参数i
 }
