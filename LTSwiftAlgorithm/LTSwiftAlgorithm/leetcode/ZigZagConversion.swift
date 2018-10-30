cc8b24c26480ba7e43447e2f6afcb211498a288b
移动支付－刘涛 <liutao900629@gmail.com>
2018-10-30 19:04:29 +0800

    new commit

--- LTSwiftAlgorithm/LTSwiftAlgorithm/leetcode/ZigZagConversion.swift
+++ LTSwiftAlgorithm/LTSwiftAlgorithm/leetcode/ZigZagConversion.swift
@@ -10,27 +10,46 @@ import Foundation
 
 class ZigZagConversion: LeetcodeSolution {
     override func solution() {
-        let s = "PAYPALISHIRING"
+        let s = "ABC"
         self.convert(s, 3)
     }
     
     func convert(_ s: String, _ numRows: Int) -> String {
-        if (numRows < 2 || s.count < 2) {
+        if (numRows < 2 || s.count <= 2) {
             return s
         }
         
-        let numberOfLines = s.count/numRows > 1 ? s.count/numRows : 1
+        var tailNumberOfLines = 0
+        if s.count%(2*numRows - 2) > numRows {
+            tailNumberOfLines = 1 + s.count%(2*numRows - 2)%numRows
+        } else if s.count%(2*numRows - 2) > 0  {
+            tailNumberOfLines = 1
+        }
+        var tailNumberOfRows = 0
+        if s.count%(2*numRows - 2) > numRows {
+            tailNumberOfRows = numRows - s.count%(2*numRows - 2)%numRows%numRows
+        } else if s.count%(2*numRows - 2) > 0  {
+            tailNumberOfRows = s.count%(2*numRows - 2)%numRows%numRows
+        }
+        
+        let numberOfLines = s.count/(2*numRows - 2)*(numRows - 1) + tailNumberOfLines
         var arr = Array(repeating: Array(repeating: " ", count: numberOfLines), count: numRows)
         var index = 0
-        for character in s {
-            if index/(numberOfLines) == 0 {
-                arr[index%(numRows)][index/(numberOfLines)] = String(character)
-            } else if (index%(numRows) + index/(numberOfLines - 1) == numRows - 1) {
-                arr[index%(numRows)][index/(numberOfLines)] = String(character)
-            } else {
-                arr[index%(numRows)][index/(numberOfLines)] = " "
+        for i in 0..<numberOfLines {
+            for j in 0..<numRows {
+                if i == 0 && j == 0 {
+                    
+                }
+                if i == numberOfLines - 1 && j >= tailNumberOfRows {
+                    continue
+                }
+                if i%(numRows-1) == 0 || i%(numRows - 1)+j == numRows - 1{
+                    arr[j][i] = String(s[String.Index(encodedOffset: index)...String.Index(encodedOffset: index)])
+                    index += 1
+                } else {
+                    arr[j][i] = " "
+                }
             }
-            index += 1
         }
         
         var result = ""
